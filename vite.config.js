92eb8e952ae25542820d5387e3c22967f12fd4b6
ArrozConPollo7 <elcrackmas882gmail.com>
2025-11-24 11:57:57 -0500

    a ver si funca

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Template1/', // Cambia 'Template1' por el nombre de tu repositorio
+  base: '/', // Root path for user GitHub Pages (ArrozConPollo7.github.io)
 })
